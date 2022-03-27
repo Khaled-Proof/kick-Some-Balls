c506701f95d06c8daa0c6fdb072d68dc376821d2
Khaled-Proof <khaledkharboutly@yahoo.com>
2022-03-27 22:41:40 +0200

    Add files via upload

---
+++ public/javascripts/drag.js
@@ -0,0 +1,102 @@
+
+// target elements with the "draggable" class
+interact('.draggable')
+  .draggable({
+    // enable inertial throwing
+    inertia: true,
+    // keep the element within the area of it's parent
+    modifiers: [
+      interact.modifiers.restrictRect({
+        restriction: 'parent',
+        endOnly: true
+      })
+    ],
+    // enable autoScroll
+    autoScroll: true,
+
+    listeners: {
+      // call this function on every dragmove event
+      move: dragMoveListener,
+
+      // call this function on every dragend event
+      end (event) {
+        var textEl = event.target.querySelector('p')
+
+        textEl && (textEl.textContent =
+          'moved a distance of ' +
+          (Math.sqrt(Math.pow(event.pageX - event.x0, 2) +
+                     Math.pow(event.pageY - event.y0, 2) | 0))
+            .toFixed(2) + 'px')
+       }
+    }
+  })
+
+function dragMoveListener (event) {
+  var target = event.target
+  // keep the dragged position in the data-x/data-y attributes
+  var x = (parseFloat(target.getAttribute('data-x')) || 0) + event.dx
+  var y = (parseFloat(target.getAttribute('data-y')) || 0) + event.dy
+
+  // translate the element
+  target.style.transform = 'translate(' + x + 'px, ' + y + 'px)'
+
+  // update the posiion attributes
+  target.setAttribute('data-x', x)
+  target.setAttribute('data-y', y)
+}
+
+// this function is used later in the resizing and gesture demos
+window.dragMoveListener = dragMoveListener
+
+
+// enable draggables to be dropped into this
+interact('.dropzone').dropzone({
+  // only accept elements matching this CSS selector
+  //accept: '#gizn, #Nico',
+  // Require a 75% element overlap for a drop to be possible
+  overlap: 0.75,
+
+  // listen for drop related events:
+
+  ondropactivate: function (event) {
+    // add active dropzone feedback
+    event.target.classList.add('drop-active')
+  },
+  ondragenter: function (event) {
+    var draggableElement = event.relatedTarget
+    var dropzoneElement = event.target
+
+    // feedback the possibility of a drop
+    dropzoneElement.classList.add('drop-target')
+    draggableElement.classList.add('can-drop')
+    //draggableElement.textContent = 'Dragged in'
+  },
+  ondragleave: function (event) {
+    // remove the drop feedback style
+    event.target.classList.remove('drop-target')
+    event.relatedTarget.classList.remove('can-drop')
+    //event.relatedTarget.textContent = 'Dragged out'
+  },
+  ondrop: function (event) {
+    event.relatedTarget.textContent = `${event.relatedTarget.id}` + ' Dropped'
+  },
+  ondropdeactivate: function (event) {
+    // remove active dropzone feedback
+    event.target.classList.remove('drop-active')
+    event.target.classList.remove('drop-target')
+  }
+})
+
+interact('.drag-drop')
+  .draggable({
+    inertia: true,
+    modifiers: [
+      interact.modifiers.restrictRect({
+        restriction: 'parent',
+        endOnly: true
+      })
+    ],
+    autoScroll: true,
+    // dragMoveListener from the dragging demo above
+    listeners: { move: dragMoveListener }
+  })
---
+++ routes/index.js
@@ -0,0 +1,261 @@
+const express = require('express');
+const router = express.Router();
+//const mongo = require('mongodb');
+const assert = require('assert');
+//const {ObjectId} = require("mongodb");
+//const {MongoClient} = require("mongodb");
+//const mongo = require('mongodb').MongoClient;
+//const mongoid = require('mongodb').ObjectId;
+const chart =require('chart.js');
+const {CanvasRenderService} = require('chartjs-node-canvas');
+//const db = require('monk')('localhost:27017/test')
+//const userdata = db.get('user-data');
+const inetract = require('interact.js');
+
+//const url = 'mongodb://localhost:27017/test'
+const mongoose = require("mongoose");
+mongoose.connect('mongodb://localhost:27017/test');
+const Schema =mongoose.Schema;
+
+const userdataSchema = new Schema({
+Player1: {type:String},
+Player2: {type:String},
+Player3: {type:String},
+Player4: {type:String},
+time: {type:String},
+team1: {type:String},
+team2: {type:String},
+},{collection:'user-data'});
+const userdata=mongoose.model('Userdata',userdataSchema);
+
+
+
+
+//Data retrive with mongoose for this you do not need mongodb
+router.get('/index', function(req, res, next) {
+
+  res.render('index',{ title: 'giZen&Khaled Kicker Project',condition:true,array:[1,2,3,4] } );
+
+
+});
+router.get('/', function(req, res, next) {
+  res.render('index',{ title: 'giZen&Khaled Kicker Project',condition:true,array:[1,2,3,4] } );
+
+
+});
+
+router.get('/get-data', function (req, res, next) {
+ // in the line belwo you can write the condtions of the data you wanna retrive in dthe finde funtion
+userdata.find().lean()
+    .then(function (doc){
+      res.render('index',{item:doc});
+    });
+});
+
+
+
+
+router.post('/insert', function(req, res, next) {
+    const id = req.body.id;
+    const  d = new Date();
+    const datetime= d.getDate() + '/' + (d.getMonth()+1) + '/' + d.getFullYear() + '-' + d.getHours() + '-' + d.getMinutes() + '-' + d.getSeconds();
+  const item = {
+    Player1: req.body.Player1,
+    Player2: req.body.Player2,
+    Player3: req.body.Player3,
+    Player4: req.body.Player4,
+      time:datetime,
+    team1: req.body.team1,
+    team2: req.body.team2
+  };
+
+const data=new userdata(item);
+data.save();
+
+res.redirect('index' );
+
+});
+
+router.post('/update', function(req, res, next) {
+
+  const id = req.body.id;
+  userdata.findById(id,function (err,doc){
+    if(err){
+      console.error('no entry found');
+    }
+        doc.Player1= req.body.Player1;
+        doc.Player2= req.body.Player2;
+        doc.Player3= req.body.Player3;
+        doc.Player4= req.body.Player4;
+        doc.team1= req.body.team1;
+        doc.team2= req.body.team2;
+        doc.save();
+  });
+  //the id turn any id to object
+  res.redirect('/index');
+
+});
+router.post('/delete', function(req, res, next) {
+  const id = req.body.id;
+  //userdata.delete({'_id':db.id(id),item});
+  userdata.findByIdAndRemove(id).exec();
+  res.redirect('/get-data');
+
+
+});
+
+router.get('/update/:id',function (req,res,next){
+  res.render('update',{output:req.params.id});
+});
+
+
+/* GET home page. Data retive with mongodb
+
+router.get('/index', function(req, res, next) {
+  res.render('index',{ title: 'giZen&Khaled Kicker Project',condition:true,array:[1,2,3,4] } );
+
+
+});
+router.get('/', function(req, res, next) {
+  res.render('index',{ title: 'giZen&Khaled Kicker Project',condition:true,array:[1,2,3,4] } );
+
+
+});
+
+router.get('/get-data', function (req, res, next) {
+const resultarray =[];
+      mongo.connect(url, function (err, client) {
+        const db = client.db('test')
+        assert.equal(null, err);
+        const cursor = db.collection('user-data').find();
+        cursor.forEach(function (doc,err){
+          assert.equal(null, err);
+          resultarray.push(doc);
+        },function (){
+          // MongoClient.close(); should chck the connetion closing again
+          res.render('index',{items:resultarray,title: 'giZen&Khaled Kicker Project',condition:true,array:[1,2,3,4] } );
+        });
+      });
+    });
+
+
+
+
+router.post('/insert', function(req, res, next) {
+  const id = req.body.id;
+  const  d = new Date();
+  const datetime= d.getDate() + '/' + (d.getMonth()+1) + '/' + d.getFullYear() + '-' + d.getHours() + '-' + d.getMinutes() + '-' + d.getSeconds();
+  let item = {
+    Player1: req.body.Player1,
+    Player2: req.body.Player2,
+    Player3: req.body.Player3,
+    Player4: req.body.Player4,
+    time: datetime,
+    team1: req.body.team1,
+    team2: req.body.team2
+  };
+
+  mongo.connect(url,function (err, client) {
+    const db = client.db('test')
+
+    assert.equal(null,err);
+
+    db.collection('user-data').insertOne(item,function (err,result){
+      assert.equal(null,err);
+      console.log('Item has been inserted');
+     // MongoClient.close(); should chck the connetion closing again
+    });
+  });
+    res.redirect('index' );
+
+  });
+
+router.post('/update', function(req, res, next) {
+  let item = {
+    player1: req.body.player1,
+    player2: req.body.player2,
+    player3: req.body.player3,
+    player4: req.body.player4,
+    team1: req.body.team1,
+    team2: req.body.team2
+  };
+  const id = req.body.id;
+  mongo.connect(url,function (err, client) {
+    const db = client.db('test')
+
+    assert.equal(null,err);
+
+    db.collection('user-data').updateOne({"_id":mongoid(id)},{$set:item},function (err,result){
+      assert.equal(null,err);
+      console.log('Item has been updated');
+      // MongoClient.close(); should chck the connetion closing again
+      res.redirect('/get-data');
+
+    });
+  });
+
+});
+router.post('/delete', function(req, res, next) {
+  const id = req.body.id;
+  mongo.connect(url,function (err, client) {
+    const db = client.db('test')
+
+    assert.equal(null,err);
+
+    db.collection('user-data').deleteOne({"_id":mongoid(id)},function (err,result){
+      assert.equal(null,err);
+      console.log('Item has been deleted');
+      res.redirect('/get-data');
+      // MongoClient.close(); should chck the connetion closing again
+    });
+  });
+
+});
+
+router.get('/update/:id',function (req,res,next){
+  res.render('update',{output:req.params.id});
+});
+
+
+
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Form Validation',success:req.session.success,errors:req.session.errors});
+  req.session.errors=null;
+  req.session.success=null;
+
+});
+router.post('/submit',function (req,res,next){
+  //check validity (validator link) https://github.com/validatorjs/validator.js#validators
+req.check('email','Invalid E-Mail Form').isEmail();
+req.check('password','Passowrd is not valid or not matching').isLength({min:4}).equals(req.body.confirmpassword);
+const errors= req.validationErrors();
+if(errors){
+  req.session.errors=errors;
+  req.session.success =false;
+}else{
+  req.session.success =true;
+
+}
+  res.redirect('/');
+});
+
+
+/* pass parametre to the url
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'G&K Tablesoccer Project',condition:true,array:[1,2,3,4] });
+});
+router.get('/test/:id',function (req,res,next){
+  res.render('test',{output:req.params.id});
+});
+*/
+/*post a value and redirect
+router.post('/test/submit',function (req,res,next){
+var id = req.body.id;
+  res.redirect('/test/'+id);
+});
+*/
+
+
+
+
+module.exports = router;
