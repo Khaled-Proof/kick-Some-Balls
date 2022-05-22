2dd90b07e9c6b712fa85072a1566b8182b1ff8a4
Khaled <khaledkharboutly@yahoo.com>
2022-05-22 04:01:18 +0200

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -15,6 +15,7 @@ const inetract = require('interact.js');
 //const url = 'mongodb://localhost:27017/test'
 const mongoose = require("mongoose");
 const Handlebars = require("handlebars");
+const path = require("path");
 mongoose.connect('mongodb://localhost:27017/test');
 const Schema =mongoose.Schema;
 
@@ -24,30 +25,66 @@ Player2: {type:String},
 Player3: {type:String},
 Player4: {type:String},
 time: {type:String},
-team1: {type:String},
-team2: {type:String},
+team1: {type:Number},
+team2: {type:Number},
 },{collection:'user-data'});
 const userdata=mongoose.model('Userdata',userdataSchema);
 
-
+const playersSchema = new Schema({
+    name: {type:String},
+    time: {type:String},
+},{collection:'players-data'});
+const playersdata=mongoose.model('Playersdata',playersSchema);
 
 
 //Data retrive with mongoose for this you do not need mongodb
+router.get('/register', function(req, res, next) {
+
+  res.render('register',{ title: 'giZen&Khaled Kicker Project',condition:true,array:[1,2,3,4] } );
+
+});
 router.get('/index', function(req, res, next) {
+    Handlebars.registerHelper('selected', function(option, value){
+        if (option === value) {
+            return ' selected';
+        } else {
+            return ''
+        }
+    });
+    playersdata.find().lean()
+        .then(function (doc){
+            res.render('index',{players:doc,title: 'giZen&Khaled Kicker Project'});
 
-  res.render('index',{ title: 'giZen&Khaled Kicker Project',condition:true,array:[1,2,3,4] } );
-
+        });
 
 });
 router.get('/', function(req, res, next) {
-  res.render('index',{ title: 'giZen&Khaled Kicker Project',condition:true,array:[1,2,3,4] } );
+    Handlebars.registerHelper('selected', function(option, value){
+        if (option === value) {
+            return ' selected';
+        } else {
+            return ''
+        }
+    });
+    playersdata.find().lean()
+        .then(function (doc){
+            res.render('index',{players:doc,title: 'giZen&Khaled Kicker Project'});
+
+        });
 
 
 });
 
 router.get('/get-data', function (req, res, next) {
  // in the line belwo you can write the condtions of the data you wanna retrive in dthe finde funtion
-userdata.find().lean()
+    Handlebars.registerHelper('selected', function(option, value){
+        if (option === value) {
+            return ' selected';
+        } else {
+            return ''
+        }
+    });
+    userdata.find().lean()
     .then(function (doc){
       res.render('index',{item:doc,title: 'giZen&Khaled Kicker Project'});
     });
@@ -56,17 +93,18 @@ userdata.find().lean()
 router.post('/insert', function(req, res, next) {
     const id = req.body.id;
     const  d = new Date();
-    const datetime= d.getDate() + '/' + (d.getMonth()+1) + '/' + d.getFullYear() + '-' + d.getHours() + '-' + d.getMinutes() + '-' + d.getSeconds();
+    const datetime= d.getDate() + '/' + (d.getMonth()+1) + '/' + d.getFullYear() + '-' + d.getHours() + ':' + d.getMinutes() + ':' + d.getSeconds();
 
     const item = {
-    Player1: req.body.Player1,
-    Player2: req.body.Player2,
-    Player3: req.body.Player3,
-    Player4: req.body.Player4,
+    Player1: req.body.Player1.toString().replace(",", ""),
+    Player2: req.body.Player2.toString().replace(",", ""),
+    Player3: req.body.Player3.toString().replace(",", ""),
+    Player4: req.body.Player4.toString().replace(",", ""),
       time:datetime,
     team1: req.body.team1,
     team2: req.body.team2,
   };
+
 
 const data=new userdata(item);
 data.save();
@@ -82,10 +120,10 @@ router.post('/update', function(req, res, next) {
     if(err){
       console.error('no entry found');
     }
-        doc.Player1= req.body.Player1;
-        doc.Player2= req.body.Player2;
-        doc.Player3= req.body.Player3;
-        doc.Player4= req.body.Player4;
+        doc.Player1= req.body.Player1.toString().replace(",", "");
+        doc.Player2= req.body.Player2.toString().replace(",", "");
+        doc.Player3= req.body.Player3.toString().replace(",", "");
+        doc.Player4= req.body.Player4.toString().replace(",", "");
         doc.team1= req.body.team1;
         doc.team2= req.body.team2;
         doc.save();
@@ -107,13 +145,26 @@ router.get('/update/:id',function (req,res,next){
   res.render('update',{output:req.params.id,title: 'giZen&Khaled Kicker Project'});
 });
 
-router.get('/charts/:Player2',async function (req, res, next) {
+router.get('/history/:Player2',async function (req, res, next) {
     const Handlebars = require('handlebars');
     Handlebars.registerHelper("inc", function (value, options) {
         return parseInt(value) + 1;
     });
     const name = req.params.Player2;
     const count = await userdata.countDocuments({$or: [{Player1: name}, {Player2: name}, {Player3: name}, {Player4: name}]});
+
+    const won1 = await userdata.countDocuments({$and: [{$or: [{ Player2: name }, { Player1: name }]}, {$or: [{ $expr: { $gt: ['$team1', '$team2'] }}]}]});
+    const won2 = await userdata.countDocuments({$and: [{$or: [{ Player3: name }, { Player4: name }]}, {$or: [{ $expr: { $gt: ['$team2', '$team1'] }}]}]});
+    const totalwon= won1+won2;
+
+    const lose1 = await userdata.countDocuments({$and: [{$or: [{ Player2: name }, { Player1: name }]}, {$or: [{ $expr: { $lt: ['$team1', '$team2'] }}]}]});
+    const lose2 = await userdata.countDocuments({$and: [{$or: [{ Player3: name }, { Player4: name }]}, {$or: [{ $expr: { $lt: ['$team2', '$team1'] }}]}]});
+    const totallose= lose1+lose2;
+    const wonperecent1 = (totalwon/(totalwon+totallose))*100;
+    const losperecent1 = (totallose/(totalwon+totallose))*100;
+
+
+
 
     Handlebars.registerHelper('ifCond', function(v1, v2, options) {
         if(v1 === v2) {
@@ -131,8 +182,126 @@ router.get('/charts/:Player2',async function (req, res, next) {
     userdata.find({$or: [{Player1: name}, {Player2: name}, {Player3: name}, {Player4: name}]}).lean()
 
         .then(function (doc) {
-            res.render('charts', {item: doc, title: 'giZen&Khaled Kicker Project',count1:count, condition: true,test1:name, array: [1, 2, 3, 4]});
+            res.render('history', {item: doc, title: 'giZen&Khaled Kicker Project',count1:count,won:totalwon,lost:totallose,wonpresent:wonperecent1,lospresent:losperecent1, condition: true,test1:name, array: [1, 2, 3, 4]});
         });
+
+});
+
+
+
+router.get('/charts',async function (req, res, next) {
+//khaled start
+    const finalResults = [];
+    const wonResults = [];
+    const lostResults = [];
+
+    const Promise = playersdata.find({ }, async function (err, results) {
+        for (const element of results) {
+            console.log(element.name);
+             name2 = element.name;
+            finalResults.push(name2);
+
+            const khaledwon1 = await userdata.countDocuments({$and: [{$or: [{ Player2: name2 }, { Player1: name2 }]}, {$or: [{ $expr: { $gt: ['$team1', '$team2'] }}]}]});
+            const khaledwon2 = await userdata.countDocuments({$and: [{$or: [{ Player3: name2 }, { Player4: name2 }]}, {$or: [{ $expr: { $gt: ['$team2', '$team1'] }}]}]});
+            const khaledtotalwon= khaledwon1+khaledwon2;
+            wonResults.push(khaledtotalwon);
+
+            const khaledlose1 = await userdata.countDocuments({$and: [{$or: [{ Player2: name2 }, { Player1: name2 }]}, {$or: [{ $expr: { $lt: ['$team1', '$team2'] }}]}]});
+            const khaledlose2 = await userdata.countDocuments({$and: [{$or: [{ Player3: name2 }, { Player4: name2 }]}, {$or: [{ $expr: { $lt: ['$team2', '$team1'] }}]}]});
+            const khaledtotallose= (khaledlose1+khaledlose2);
+            lostResults.push(-khaledtotallose);
+
+            const khaledtotal =khaledtotallose+khaledtotalwon;
+
+            console.log(khaledtotalwon)
+            console.log(khaledtotal);
+
+        }
+        const wontot = JSON.stringify(finalResults)
+      //  khaledwon:khaledtotalwon,khaledlost:khaledtotallose,totalg:khaledtotal,
+        res.render('charts', {title: 'giZen&Khaled Kicker Project', condition: true, namearray: finalResults,wonarray:wonResults,lostarray:lostResults
+        });
+
+
+    });
+
+
+
+/**
+    const khaledwon1 = await userdata.countDocuments({$and: [{$or: [{ Player2: 'Khaled' }, { Player1: 'Khaled' }]}, {$or: [{ $expr: { $gt: ['$team1', '$team2'] }}]}]});
+    const khaledwon2 = await userdata.countDocuments({$and: [{$or: [{ Player3: 'Khaled' }, { Player4: 'Khaled' }]}, {$or: [{ $expr: { $gt: ['$team2', '$team1'] }}]}]});
+    const khaledtotalwon= khaledwon1+khaledwon2;
+
+    const khaledlose1 = await userdata.countDocuments({$and: [{$or: [{ Player2: 'Khaled' }, { Player1: 'Khaled' }]}, {$or: [{ $expr: { $lt: ['$team1', '$team2'] }}]}]});
+    const khaledlose2 = await userdata.countDocuments({$and: [{$or: [{ Player3: 'Khaled' }, { Player4: 'Khaled' }]}, {$or: [{ $expr: { $lt: ['$team2', '$team1'] }}]}]});
+    const khaledtotallose= khaledlose1+khaledlose2;
+    const khaledtotal =khaledtotallose+khaledtotalwon;
+    //khaled end
+**/
+//giZen start
+    const giZenwon1 = await userdata.countDocuments({$and: [{$or: [{ Player2: 'giZen' }, { Player1: 'giZen' }]}, {$or: [{ $expr: { $gt: ['$team1', '$team2'] }}]}]});
+    const giZenwon2 = await userdata.countDocuments({$and: [{$or: [{ Player3: 'giZen' }, { Player4: 'giZen' }]}, {$or: [{ $expr: { $gt: ['$team2', '$team1'] }}]}]});
+    const giZentotalwon= giZenwon1+giZenwon2;
+
+    const giZenlose1 = await userdata.countDocuments({$and: [{$or: [{ Player2: 'giZen' }, { Player1: 'giZen' }]}, {$or: [{ $expr: { $lt: ['$team1', '$team2'] }}]}]});
+    const giZenlose2 = await userdata.countDocuments({$and: [{$or: [{ Player3: 'giZen' }, { Player4: 'giZen' }]}, {$or: [{ $expr: { $lt: ['$team2', '$team1'] }}]}]});
+    const giZentotallose= giZenlose1+giZenlose2;
+    //giZen end
+
+    //Nico start
+    const Nicowon1 = await userdata.countDocuments({$and: [{$or: [{ Player2: 'Nico' }, { Player1: 'Nico' }]}, {$or: [{ $expr: { $gt: ['$team1', '$team2'] }}]}]});
+    const Nicowon2 = await userdata.countDocuments({$and: [{$or: [{ Player3: 'Nico' }, { Player4: 'Nico' }]}, {$or: [{ $expr: { $gt: ['$team2', '$team1'] }}]}]});
+    const Nicototalwon= Nicowon1+Nicowon2;
+
+    const Nicolose1 = await userdata.countDocuments({$and: [{$or: [{ Player2: 'Nico' }, { Player1: 'Nico' }]}, {$or: [{ $expr: { $lt: ['$team1', '$team2'] }}]}]});
+    const Nicolose2 = await userdata.countDocuments({$and: [{$or: [{ Player3: 'Nico' }, { Player4: 'Nico' }]}, {$or: [{ $expr: { $lt: ['$team2', '$team1'] }}]}]});
+    const Nicototallose= Nicolose1+Nicolose2;
+    //Nico end
+    //Nico start
+    const Davidwon1 = await userdata.countDocuments({$and: [{$or: [{ Player2: 'David' }, { Player1: 'David' }]}, {$or: [{ $expr: { $gt: ['$team1', '$team2'] }}]}]});
+    const Davidwon2 = await userdata.countDocuments({$and: [{$or: [{ Player3: 'David' }, { Player4: 'David' }]}, {$or: [{ $expr: { $gt: ['$team2', '$team1'] }}]}]});
+    const Davidtotalwon= Davidwon1+Davidwon2;
+
+    const Davidlose1 = await userdata.countDocuments({$and: [{$or: [{ Player2: 'David' }, { Player1: 'David' }]}, {$or: [{ $expr: { $lt: ['$team1', '$team2'] }}]}]});
+    const Davidlose2 = await userdata.countDocuments({$and: [{$or: [{ Player3: 'David' }, { Player4: 'David' }]}, {$or: [{ $expr: { $lt: ['$team2', '$team1'] }}]}]});
+    const Davidtotallose= Davidlose1+Davidlose2;
+    //Nico end
+  //  res.render('charts', { title: 'giZen&Khaled Kicker Project',khaledwon:khaledtotalwon,khaledlost:khaledtotallose,giZenwon:giZentotalwon,giZenlost:giZentotallose,Nicowon:Nicototalwon,Nicolost:Nicototallose,Davidwon:Davidtotalwon,Davidlost:Davidtotallose, condition: true, array: [1, 2, 3, 4]});
+
+
+});
+
+
+router.post('/register', function(req, res, next) {
+   // const id = req.body.id;
+    const  d = new Date();
+    const datetime= d.getDate() + '/' + (d.getMonth()+1) + '/' + d.getFullYear() + '-' + d.getHours() + '-' + d.getMinutes() + '-' + d.getSeconds();
+
+    const players = {
+        name: req.body.name,
+        time:datetime,
+    };
+    const data=new playersdata(players);
+    data.save();
+
+    res.redirect('get-players' );
+
+});
+
+router.get('/get-players', function (req, res, next) {
+    // in the line belwo you can write the condtions of the data you wanna retrive in dthe finde funtion
+    playersdata.find().lean()
+        .then(function (doc){
+            res.render('register',{item:doc,title: 'giZen&Khaled Kicker Project'});
+        });
+});
+
+router.post('/delete-player', function(req, res, next) {
+    const id = req.body.id;
+    //userdata.delete({'_id':db.id(id),item});
+    playersdata.findByIdAndRemove(id).exec();
+    res.redirect('/get-players');
+
 
 });
 
